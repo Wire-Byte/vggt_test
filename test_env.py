bdbbb36c572cb33e8679e8c9fc46fd7980a9b453
shiyc <1985875241@qq.com>
2026-03-13 22:19:39 +0800

    finish_first_test

--- test_env.py
+++ test_env.py
@@ -2,13 +2,14 @@ import torch
 from vggt.models.vggt import VGGT
 from vggt.utils.load_fn import load_and_preprocess_images
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
+device = "cuda:7" if torch.cuda.is_available() else "cpu"
 # bfloat16 is supported on Ampere GPUs (Compute Capability 8.0+)
 dtype = torch.bfloat16 if torch.cuda.get_device_capability()[0] >= 8 else torch.float16
 
-# Initialize the model and load the pretrained weights.
-# This will automatically download the model weights the first time it's run, which may take a while.
-model = VGGT.from_pretrained("facebook/VGGT-1B").to(device)
+# Initialize the model and load the pretrained weights from local file
+model = VGGT()
+model.load_state_dict(torch.load("checkpoints/model.pt", map_location=device))
+model = model.to(device)
 
 # Load and preprocess example images
 image_names = [
